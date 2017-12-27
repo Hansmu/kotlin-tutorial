7b8979ef480c542fb64839bc5588351cb3d01a6c
hansmu <hans.miikael@gmail.com>
2017-12-27 12:57:51 +0200

    Imports comment

--- src/main/kotlin/com/github/kotlintutorial/HelloWorld.kt
+++ src/main/kotlin/com/github/kotlintutorial/HelloWorld.kt
@@ -1,5 +1,7 @@
 package com.github.kotlintutorial
 
+// No imports for array and such because Kotlin imports a whole bunch of stuff by default from the Kotlin standard library. It's to cut down on boilerplate.
+
 // Functions outside of classes are top level functions
 fun main(args: Array<String>) {
     //  The code isn't actually outside of a class. It's just when we're writing the code we don't have to have it in a class.
