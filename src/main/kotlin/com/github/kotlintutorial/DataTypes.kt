d66d46fcb4eddaa38596ea81c866f8dcd5c946a3
hansmu <hans.miikael@gmail.com>
2017-12-28 11:56:29 +0200

    Arrays example

--- src/main/kotlin/com/github/kotlintutorial/DataTypes.kt
+++ src/main/kotlin/com/github/kotlintutorial/DataTypes.kt
@@ -1,6 +1,7 @@
 package com.github.kotlintutorial
 
 import com.github.kotlintutorial.javacode.DummyClass
+import java.math.BigDecimal
 
 fun main(args: Array<String>) {
     // Data types are the same as in Java because Kotlin builds for the JVM
@@ -44,4 +45,31 @@ fun main(args: Array<String>) {
     // Unit class is same as the void class in Java. In Java void isn't really a data type. Kotlin uses the Unit class for a function that doesn't return anything.
     // Unit actually exists in Kotlin. It returns a singleton Unit instance.
     // Kotlin has a Nothing class. You can use it to return from a function that'll never actually return. Ex infinite loop
+
+    val names = arrayOf("John", "Jane", "Jill", "Bob")
+    val longs = arrayOf(1L, 2L, 3L)
+    val longs2 = arrayOf<Long>(1, 2, 3, 4)
+
+    println("Regular array indexing can be used with lists ${names[2]}")
+
+    val evenNumbers = Array(16) { i -> i * 2 }
+
+    for (number in evenNumbers) {
+        println(number)
+    }
+
+    val allZeroes = Array(100) { 0 }
+
+    // With no value to initialize, you have to specify what type the array will be
+    var someArray: Array<Int>
+    someArray = arrayOf(1, 2, 3, 4)
+
+    val mixedArray = arrayOf("hello", 22, BigDecimal(10.5), 'a')
+
+    println(mixedArray is Array<Any>)
+
+    // With arrays you can't call a method that wants a primitive type array as a parameter. You have to use the specific array method.
+    // You get a performance boost by using the special array methods, because these special arrays will be treated as primitive type arrays instead of lists.
+    DummyClass().printNumbers(intArrayOf(1, 2, 3))
+    DummyClass().printNumbers(evenNumbers.toIntArray())
 }
--- src/main/kotlin/com/github/kotlintutorial/javacode/DummyClass.java
+++ src/main/kotlin/com/github/kotlintutorial/javacode/DummyClass.java
@@ -4,4 +4,10 @@ public class DummyClass {
     public String isVacationTime(boolean onVacation) {
         return onVacation ? "Is on vacation" : "At work";
     }
+
+    public void printNumbers(int[] numbers) {
+        for (int number: numbers) {
+            System.out.println(number);
+        }
+    }
 }
